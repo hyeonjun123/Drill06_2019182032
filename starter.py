51096121349132bbd6ee9a11b020da0b572a1077
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-10-01 18:35:51 +0900

    클릭한 좌표값 리스트에 저장, 캐릭터가 직선이동으로 움직이는 마우스 따라오도록

--- starter.py
+++ starter.py
@@ -10,6 +10,7 @@ hand_arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
+    global x, y
     global x, y,x1,y1
     events = get_events()
     for event in events:
@@ -18,19 +19,19 @@ def handle_events():
         elif event.type ==SDL_MOUSEBUTTONDOWN:
             x1, y1 = event.x, TUK_HEIGHT -1 - event.y
             click_list.append((x1,y1))
-
+            print(click_list)
+            
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-
-    pass
 
 
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 x1,y1 = 0,0
+character_x, character_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 click_list = []
 
 frame = 0
@@ -39,8 +40,23 @@ hide_cursor()
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+
     hand_arrow.clip_draw(0,0,50,50,x,y)
     hand_arrow.clip_draw(0,0,50,50,x1,y1)
+
+
+    dx = x - character_x
+    dy = y - character_y
+    distance = math.sqrt(dx ** 2 + dy ** 2)
+
+    if distance > 0:
+        speed = 0.5
+        character_x += (dx / distance) * speed
+        character_y += (dy / distance) * speed
+
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
+    update_canvas()
+    frame = (frame + 1) % 8
 
     #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
